384ca3fdfbcf8a4a0e9314b8cd31e6089365252c
busycalibrating <19653980+busycalibrating@users.noreply.github.com>
2021-11-11 14:07:18 -0500

    ...now?

--- cuda.sh
+++ cuda.sh
@@ -4,7 +4,6 @@
 # https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html
 
 # Warning - probably out of date by now!
-# Another warning woo
 echo Installing cuda 11.4.2
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600

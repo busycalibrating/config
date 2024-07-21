9fb9b8978de3c00464f0e43c25c29491007a3494
busycalibrating <19653980+busycalibrating@users.noreply.github.com>
2024-07-20 22:07:51 -0400

    add flac2aiff

---
+++ .config/fish/functions/flac2aiff.fish
@@ -0,0 +1,41 @@
+function flac2aiff
+    argparse 'y/yes' 'd/directory=' -- $argv
+    or return
+
+    set dir $_flag_directory
+    if test -z "$dir"
+        set dir "."
+    end
+
+    set ffmpeg_opts "-n"
+    if set -q _flag_yes
+        set ffmpeg_opts "-y"
+    end
+
+    for file in $dir/*.flac
+        set basename (basename $file .flac)
+        ffmpeg $ffmpeg_opts -i $file  -write_id3v2 1 -c:v copy "$dir/$basename.aiff"
+    end
+end
+# function flac2wav
+#     argparse 'y/yes' 'n/no' 'd/directory=' -- $argv
+#     or return
+# 
+#     set dir $_flag_directory
+#     if test -z "$dir"
+#         set dir "."
+#     end
+# 
+#     set ffmpeg_opts ""
+#     if set -q _flag_yes
+#         set ffmpeg_opts "-y"
+#     else if set -q _flag_no
+#         set ffmpeg_opts "-n"
+#     end
+# 
+#     for file in $dir/*.flac
+#         set basename (basename $file .flac)
+#         echo ffmpeg $ffmpeg_opts -i $file "$dir/$basename.wav"
+#         ffmpeg $ffmpeg_opts -i $file "$dir/$basename.wav"
+#     end
+# end
